b373960708217203b2503a3b80589d461d675003
IagoOliveira23 <iago.ishift@gmail.com>
2023-09-01 09:40:09 -0300

    construindo o build do component

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -1,10 +1,42 @@
-class CardNews extends HTMLElement{
+class Cardnews extends HTMLElement{
     constructor(){
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Olá, mundo</h1>";
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const autor = document.createElement("span");
+        const titulo = document.createElement("a")
+        const texto = document.createElement("p")
+
+        cardLeft.appendChild(autor)
+        cardLeft.appendChild(titulo)
+        cardLeft.appendChild(texto)
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const imagem = document.createElement("img");
+        
+        cardRight.appendChild(imagem);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+        return componentRoot;
+    }
+
+    styles(){
+
     }
 }
 
-customElements.define('card-news', CardNews);
+customElements.define("card-news", Cardnews);
